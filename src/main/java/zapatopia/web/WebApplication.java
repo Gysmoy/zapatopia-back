de77c14a38bba5339cb8e144807db2a622abd679
LuisAUH <libra935670072@gmail.com>
2024-05-20 23:50:43 -0500

    NTS

--- src/main/java/zapatopia/web/WebApplication.java
+++ src/main/java/zapatopia/web/WebApplication.java
@@ -1,13 +1,27 @@
 package zapatopia.web;
 
+import com.github.ydespreaux.spring.data.jpa.repository.config.EnableJpaCriteriaRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import zapatopia.web.services.AuditorAwareImpl;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"zapatopia.web.jpa", "zapatopia.web.controller", "zapatopia.web.repository", "zapatopia.web.services"})
 public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-	}
-
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableJpaCriteriaRepositories(basePackages = {"zapatopia.web.repository"})
+class JpaConfiguration {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
 }
