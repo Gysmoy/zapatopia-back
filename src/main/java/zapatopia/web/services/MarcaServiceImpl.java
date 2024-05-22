14eff6d95a91418f5c8041957b86238461603070
LuisAUH <libra935670072@gmail.com>
2024-05-22 00:28:47 -0500

    NTS

--- src/main/java/zapatopia/web/services/MarcaServiceImpl.java
+++ src/main/java/zapatopia/web/services/MarcaServiceImpl.java
@@ -1,10 +1,14 @@
 package zapatopia.web.services;
 
+import com.mysql.cj.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import zapatopia.web.jpa.MarcaJpa;
 import zapatopia.web.repository.MarcaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -12,6 +16,9 @@ public class MarcaServiceImpl implements MarcaService {
 
     @Autowired
     private MarcaRepository marcaRepository;
+
+    final Logger logger = LoggerFactory.getLogger(MarcaServiceImpl.class);
+
 
     @Override
     public List<MarcaJpa> obtenerMarcas() {
@@ -21,5 +28,20 @@ public class MarcaServiceImpl implements MarcaService {
     @Override
     public MarcaJpa obtenerMarca(long id) {
         return marcaRepository.findById(id).isPresent() ? marcaRepository.findById(id).get() : null;
+    }
+
+    @Override
+    public MarcaJpa guardarMarca(MarcaJpa marca) {
+        if (marca.getId() == 0) {
+            logger.info("Creando nueva marca");
+            marca.setFechaCreacion(LocalDateTime.now());
+        }
+        marca.setFechaModificacion(LocalDateTime.now());
+        return marcaRepository.save(marca);
+    }
+
+    @Override
+    public void eliminarMarca(long id) {
+        marcaRepository.deleteById(id);
     }
 }
