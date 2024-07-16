a9180d32635974996d5f67162a54e1e8a855298d
LuisAUH <libra935670072@gmail.com>
2024-07-15 23:44:10 -0500

    fix correo ofertas

--- src/main/java/zapatopia/web/config/DataInitializer.java
+++ src/main/java/zapatopia/web/config/DataInitializer.java
@@ -37,12 +37,6 @@ public class DataInitializer implements CommandLineRunner {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private MarcaRepository marcaRepository;
-
-    @Value("${imagenes.path}")
-    private String pathImagenes;
-
     @Override
     public void run(String... args) {
         if (!defaultRecordsExist()) {
@@ -92,70 +86,6 @@ public class DataInitializer implements CommandLineRunner {
         defaultUser.setFechaModificacion(LocalDateTime.now());
         usuarioRepository.save(defaultUser);
 
-        //crearMarcas();
     }
-
-    @PostConstruct
-    private void crearCarpetaImagenes() {
-        Path path = Paths.get(pathImagenes);
-
-        if (!Files.exists(path)) {
-            try {
-                Path imagenes = Files.createDirectory(path);
-                log.info("se creo carpeta: {}", imagenes.toAbsolutePath());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-          log.info("Ya existe carpeta: {}", path.toAbsolutePath());
-        }
-    }
-
-
-    private void crearMarcas() {
-
-
-        List<MarcaJpa> marcas = new ArrayList<>();
-
-        // Crear marcas
-        MarcaJpa defaultMarcaNike = new MarcaJpa();
-        defaultMarcaNike.setMarca("Nike");
-        defaultMarcaNike.setDescripcion("Nike");
-        defaultMarcaNike.setFechaCreacion(LocalDateTime.now());
-        defaultMarcaNike.setFechaModificacion(LocalDateTime.now());
-        marcas.add(defaultMarcaNike);
-
-        MarcaJpa defaultMarcaAdidas = new MarcaJpa();
-        defaultMarcaAdidas.setMarca("Adidas");
-        defaultMarcaAdidas.setDescripcion("Adidas");
-        defaultMarcaAdidas.setFechaCreacion(LocalDateTime.now());
-        defaultMarcaAdidas.setFechaModificacion(LocalDateTime.now());
-        marcas.add(defaultMarcaAdidas);
-
-        MarcaJpa defaultMarcaPuma = new MarcaJpa();
-        defaultMarcaPuma.setMarca("Puma");
-        defaultMarcaPuma.setDescripcion("Puma");
-        defaultMarcaPuma.setFechaCreacion(LocalDateTime.now());
-        defaultMarcaPuma.setFechaModificacion(LocalDateTime.now());
-        marcas.add(defaultMarcaPuma);
-
-        MarcaJpa defaultMarcaConverse = new MarcaJpa();
-        defaultMarcaConverse.setMarca("Converse");
-        defaultMarcaConverse.setDescripcion("Converse");
-        defaultMarcaConverse.setFechaCreacion(LocalDateTime.now());
-        defaultMarcaConverse.setFechaModificacion(LocalDateTime.now());
-        marcas.add(defaultMarcaConverse);
-
-        MarcaJpa defaultMarcaVans = new MarcaJpa();
-        defaultMarcaVans.setMarca("Vans");
-        defaultMarcaVans.setDescripcion("Vans");
-        defaultMarcaVans.setFechaCreacion(LocalDateTime.now());
-        defaultMarcaVans.setFechaModificacion(LocalDateTime.now());
-        marcas.add(defaultMarcaVans);
-
-        marcaRepository.saveAll(marcas);
-
-    }
-
 
 }
